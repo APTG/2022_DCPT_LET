7b2dfa2349ad597b8be3b95d144eb17d5fb0a54e
VilladsJacobsen <villads.lundsteen@gmail.com>
2022-12-05 13:55:21 +0100

    updated plotting files

--- notebooks/topas_analysis.py
+++ notebooks/topas_analysis.py
@@ -3,27 +3,43 @@ import numpy as np
 from topas2numpy import BinnedResult
 
 files = []
-files.append(BinnedResult('path to data'))
-files.append(BinnedResult('path to data'))
-for data in files:
-    print('{0} [{1}]'.format(data.quantity, data.unit))
-    print(f'Statistics: {data.statistics}')
-    for dim in data.dimensions:
-        print('{0} [{1}]: {2} bins'.format(dim.name, dim.unit, dim.n_bins))
+Dose = BinnedResult('data/topas/results/100Gy/Scoring_protonD_ZBox.csv')
+LET = BinnedResult('data/topas/results/100Gy/Scoring_protonLET_ZBox.csv')
 
-    ax = plt.subplot(111)
-    z = data.dimensions[1].get_bin_centers()
-    plt.plot(np.flip(z), np.squeeze(data.data['Sum']))
-    plt.xlabel(data.dimensions[1].name +": ["+ data.dimensions[1].unit+ "]")
-    plt.grid()
-    plt.ylabel(data.quantity +": ["+ data.unit + "]")
+ax1 = plt.subplot()
+ax2 = ax1.twinx()
 
-    plt.fill_between(x=[0, 10], y1=0, y2=max(np.squeeze(data.data['Sum'])), facecolor='blue', alpha=0.1, label="solid water")
-    plt.fill_between(x=[10.0, 10.5], y1=0, y2=max(np.squeeze(data.data['Sum'])), facecolor='black', alpha=0.3, label="pmma")
-    plt.fill_between(x=[10.5, 20.5], y1=0, y2=max(np.squeeze(data.data['Sum'])), facecolor='blue', alpha=0.1)
-    
-    plt.legend()
+x = Dose.dimensions[1].get_bin_centers()
+yLET = np.squeeze(LET.data['Sum'])
+yDose = np.squeeze(Dose.data['Sum'])
 
-    # plt.vlines(x =[11.8, 12.8], ymin = 0, ymax =max(np.squeeze(dose.data['Sum'])), color = 'red', label = 'Cellflask 3')
-    plt.savefig("TOPAS/Plots/2GySOBP/" + data.quantity)
-    plt.clf()
+scale = 2/np.mean(yDose[90:225])
+
+Dplot = ax1.plot(np.flip(x), yDose*scale,label=Dose.quantity)
+
+DplotLET = ax1.plot(np.flip(x), yDose*scale + yLET*0.05*yDose*scale, c='b' ,label="LET optimised Dose")
+Dose_uBound = yDose*scale + yLET*0.05*yDose*1.1*scale
+Dose_lBound = yDose*scale + yLET*0.05*yDose*0.9*scale
+
+Dplot_u = ax1.plot(np.flip(x), Dose_uBound, '--', c='b')
+Dplot_l = ax1.plot(np.flip(x), Dose_lBound, '--', c='b')
+
+LETplot = ax2.plot(np.flip(x), yLET, c='k', label=LET.quantity)
+ax1.set_xlabel(Dose.dimensions[1].name +": ["+ Dose.dimensions[1].unit+ "]")
+ax1.grid()
+ax1.set_ylabel(Dose.quantity +": ["+ Dose.unit + "]")
+ax2.set_ylabel(LET.quantity +": ["+ LET.unit + "]")
+
+
+ax1.fill_between(x=[0, 10], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='blue', alpha=0.1)
+ax1.fill_between(x=[10.0, 10.5], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='black', alpha=0.3)
+ax1.fill_between(x=[10.5, 20.5], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='blue', alpha=0.1)
+
+# added these three lines
+lns = LETplot+DplotLET+Dplot
+labs = [l.get_label() for l in lns]
+ax1.legend(lns, labs, loc=0)
+# plt.vlines(x =[11.8, 12.8], ymin = 0, ymax =max(np.squeeze(dose.data['Sum'])), color = 'red', label = 'Cellflask 3')
+plt.show()
+# plt.savefig("TOPAS/Plots/2GySOBP/" + data.quantity)
+
