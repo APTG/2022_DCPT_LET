65db5475298ed9830ae0974be88a8e6acf05e1dd
VilladsJacobsen <villads.lundsteen@gmail.com>
2022-12-07 14:25:06 +0100

    updated results

--- notebooks/topas_plan1a.py
+++ notebooks/topas_plan1a.py
@@ -15,30 +15,29 @@ yDose = np.squeeze(Dose.data['Sum'])
 
 scale = 2/np.mean(yDose[90:225])
 
-Dplot = ax1.plot(np.flip(x), yDose*scale,label=Dose.quantity)
+Dplot = ax1.plot(np.flip(x), yDose*scale,label="Dose")
 
-DplotLET = ax1.plot(np.flip(x), yDose*scale + yLET*0.05*yDose*scale, c='b' ,label="LET optimised Dose")
+DplotLET = ax1.plot(np.flip(x), yDose*scale + yLET*0.05*yDose*scale, c='b' ,label="RBE")
 Dose_uBound = yDose*scale + yLET*0.05*yDose*1.1*scale
 Dose_lBound = yDose*scale + yLET*0.05*yDose*0.9*scale
 
-Dplot_u = ax1.plot(np.flip(x), Dose_uBound, '--', c='b')
+Dplot_u = ax1.plot(np.flip(x), Dose_uBound, '--', c='b', label="+/-10% in LET")
 Dplot_l = ax1.plot(np.flip(x), Dose_lBound, '--', c='b')
 
-LETplot = ax2.plot(np.flip(x), yLET, c='k', label=LET.quantity)
-ax1.set_xlabel(Dose.dimensions[1].name +": ["+ Dose.dimensions[1].unit+ "]")
+LETplot = ax2.plot(np.flip(x), yLET, c='k', label="LETd")
+ax1.set_xlabel("Z" +": ["+ Dose.dimensions[1].unit+ "]")
 ax1.grid()
-ax1.set_ylabel(Dose.quantity +": ["+ Dose.unit + "]")
-ax2.set_ylabel(LET.quantity +": ["+ LET.unit + "]")
+ax1.set_ylabel("Dose" +": ["+ Dose.unit + "]")
+ax2.set_ylabel(LET.quantity +": [keV/micron]")
 
+# added these three lines
+lns = LETplot+DplotLET+Dplot+Dplot_u
+labs = [l.get_label() for l in lns]
+ax1.legend(lns, labs, loc=2)
 
 ax1.fill_between(x=[0, 10], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='blue', alpha=0.1)
 ax1.fill_between(x=[10.0, 10.5], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='black', alpha=0.3)
 ax1.fill_between(x=[10.5, 20.5], y1=0, y2=max(np.squeeze(Dose.data['Sum']))*scale+1, facecolor='blue', alpha=0.1)
-
-# added these three lines
-lns = LETplot+DplotLET+Dplot
-labs = [l.get_label() for l in lns]
-ax1.legend(lns, labs, loc=0)
 # plt.vlines(x =[11.8, 12.8], ymin = 0, ymax =max(np.squeeze(dose.data['Sum'])), color = 'red', label = 'Cellflask 3')
 # plt.show()
 plt.savefig("data/topas/results/plan_1a/" + Dose.quantity +"_" + LET.quantity)
