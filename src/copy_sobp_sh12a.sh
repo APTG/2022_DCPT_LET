bb61fc6cce28748821fb9252a722ef1ad02c1e1f
Niels Bassler <bassler@clin.au.dk>
2022-12-09 16:24:50 +0100

    fix very silly mistake.

--- src/copy_sobp_sh12a.sh
+++ src/copy_sobp_sh12a.sh
@@ -2,12 +2,12 @@
 
 exe="cp -v"
 
-$exe ./data/sh12a/input/plan_1a/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
-$exe ./data/sh12a/input/plan_1b/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
-$exe ./data/sh12a/input/plan_1c/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
+$exe ./data/resources/plans/plan1_sobp/sobp.dat ./data/sh12a/input/plan_1a/sobp.dat
+$exe ./data/resources/plans/plan1_sobp/sobp.dat ./data/sh12a/input/plan_1b/sobp.dat
+$exe ./data/resources/plans/plan1_sobp/sobp.dat ./data/sh12a/input/plan_1c/sobp.dat
 
-$exe ./data/resources/plans/plan2_160MeV/sobp.dat ./data/sh12a/input/plan_2/sobp.dat
+$exe ./data/sh12a/input/plan_2/sobp.dat ./data/resources/plans/plan2_160MeV/sobp.dat
 
-$exe ./data/sh12a/input/plan_3i/sobp.dat ./data/resources/plans/plan3_ramped/reference/sobp_ref.dat
-$exe ./data/sh12a/input/plan_3j/sobp.dat ./data/resources/plans/plan3_ramped/ramp_10cm/sobp_ramp_10cm.dat
-$exe ./data/sh12a/input/plan_3k/sobp.dat ./data/resources/plans/plan3_ramped/ramp_4cm/sobp_ramp_4cm.dat
+$exe ./data/resources/plans/plan3_ramped/reference/sobp_ref.dat ./data/sh12a/input/plan_3i/sobp.dat
+$exe ./data/resources/plans/plan3_ramped/ramp_10cm/sobp_ramp_10cm.dat ./data/sh12a/input/plan_3j/sobp.dat
+$exe ./data/resources/plans/plan3_ramped/ramp_4cm/sobp_ramp_4cm.dat ./data/sh12a/input/plan_3k/sobp.dat
