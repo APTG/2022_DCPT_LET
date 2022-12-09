580651ef69075ab0b5af8bbdb7b401c35cda4d89
Niels Bassler <bassler@clin.au.dk>
2022-12-09 14:04:11 +0100

    add copy sobp->sh12a script

---
+++ src/README.md
@@ -0,0 +1,13 @@
+# Description of scripts
+
+
+## `make_sobp.sh`
+Exports all DICOM plans to sobp.dat type files in their respective directories.
+Execute this script from project directory root.
+`src/make_sobp.sh`
+
+
+## `copy_sobp_sh12a.sh`
+Copies all plan sobp.dat type files to the respective SH12A input file directories.
+Execute this script from project directory root.
+`src/copy_sobp_sh12a.sh`
---
+++ src/copy_sobp_sh12a.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+exe=${cp -v}
+
+$exe ./data/sh12a/input/plan_1a/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
+$exe ./data/sh12a/input/plan_1b/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
+$exe ./data/sh12a/input/plan_1c/sobp.dat ./data/resources/plans/plan1_sobp/sobp.dat
+
+$exe ./data/resources/plans/plan2_160MeV/sobp.dat ./data/sh12a/input/plan_2/sobp.dat
+
+$exe ./data/sh12a/input/plan_3i/sobp.dat ./data/resources/plans/plan3_ramped/reference/sobp_ref.dat
+$exe ./data/sh12a/input/plan_3j/sobp.dat ./data/resources/plans/plan3_ramped/ramp_10cm/sobp_ramp_10cm.dat
+$exe ./data/sh12a/input/plan_3k/sobp.dat ./data/resources/plans/plan3_ramped/ramp_4cm/sobp_ramp_4cm.dat
