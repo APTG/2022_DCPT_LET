7883bfe56acf1ba8c507915495614bfca76973a8
Leszek Grzanka <leszek.grzanka@gmail.com>
2022-12-12 20:33:37 +0100

    improved script

--- src/fetch_results_from_zenodo.py
+++ src/fetch_results_from_zenodo.py
@@ -1,28 +1,33 @@
 import json
 from pathlib import Path
 import urllib.request
+import zipfile
 
 ZENODO_RECORD_ID = 7422361
 
+files_and_dest_paths = {
+    'topas.zip' : 'data/topas/results',
+    'shieldhit.zip': 'data/shieldhit/results'
+}
+
 if __name__ == '__main__':
-    # Get the list of all the files in the Zenodo repository
-    # using pure urllib library
+
+    # Get the list of all the files in the Zenodo repository using pure urllib library
     url = 'https://zenodo.org/api/records/{}'.format(ZENODO_RECORD_ID)
+    print(f"Fetching the list of files from Zenodo repository: {url}")
     with urllib.request.urlopen(url) as response:
         data = json.loads(response.read().decode('utf-8'))
         files = data['files']
         
-        # check if topas.zip is in the list of files
-        topas_results_filename = 'topas.zip'
-        path_to_location_of_this_script = Path(__file__).parent.absolute()
-        topas_results_dest_dir = Path(path_to_location_of_this_script.parent, 'data', 'topas', 'results')
-        if topas_results_filename in (f['key'] for f in files):
-            url = f'https://zenodo.org/record/{ZENODO_RECORD_ID}/files/{topas_results_filename}'
-            # download the file into temporary directory
-            path_to_temp_location = urllib.request.urlretrieve(url)
-            print(path_to_temp_location)
-            # unzip into `topas_results_dest_dir`
-            import zipfile
-            with zipfile.ZipFile(path_to_temp_location[0], 'r') as zip_ref:
-                zip_ref.extractall(topas_results_dest_dir)
-
+        for results_filename, results_dest_dir in files_and_dest_paths.items():
+            print(f"Checking if {results_filename} is in the list of files")
+            # check if topas.zip is in the list of files
+            if results_filename in (f['key'] for f in files):
+                url = f'https://zenodo.org/record/{ZENODO_RECORD_ID}/files/{results_filename}'
+                # download the file into temporary directory
+                path_to_temp_location = urllib.request.urlretrieve(url)
+                print(f"Downloaded {results_filename} to {path_to_temp_location[0]}")
+                unpacking_dir = Path(Path(__file__).parent.absolute(), results_dest_dir)
+                with zipfile.ZipFile(path_to_temp_location[0], 'r') as zip_ref:
+                    print(f"Unpacking {results_filename} to {unpacking_dir}")
+                    zip_ref.extractall(unpacking_dir)
