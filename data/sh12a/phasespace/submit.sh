feca406e06fc633c55e7bcda132ae2b497416f16
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-06-24 00:09:56 +0200

    submit

---
+++ data/sh12a/phasespace/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+generatemc -p 1000000 -j 100 . -x beam_plan1.dat detect_plan1.dat plan1.dat -e ["--beamfile=beam_plan1.dat --detectfile=detect_plan1.dat"] -s ["-p plgrid"]
+generatemc -p 1000000 -j 100 . -x beam_plan2.dat detect_plan1.dat plan2.dat -e ["--beamfile=beam_plan2.dat --detectfile=detect_plan2.dat"] -s ["-p plgrid"]
+generatemc -p 1000000 -j 100 . -x beam_plan3.dat detect_plan1.dat plan3.dat -e ["--beamfile=beam_plan3.dat --detectfile=detect_plan3.dat"] -s ["-p plgrid"]
