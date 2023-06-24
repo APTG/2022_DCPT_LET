98adc5aa6d399f5c62b5063625d9ffe171f7d473
Leszek Grzanka <leszek.grzanka@gmail.com>
2023-06-24 23:42:15 +0200

    submit

--- data/sh12a/phasespace/submit.sh
+++ data/sh12a/phasespace/submit.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-generatemc -p 1000000 -j 100 . -x beam_plan1.dat detect_plan1.dat plan1.dat -e ["--beamfile=beam_plan1.dat --detectfile=detect_plan1.dat"] -s ["-p plgrid"]
-sleep 1
-generatemc -p 1000000 -j 100 . -x beam_plan2.dat detect_plan2.dat plan2.dat -e ["--beamfile=beam_plan2.dat --detectfile=detect_plan2.dat"] -s ["-p plgrid"]
-sleep 1
-generatemc -p 1000000 -j 100 . -x beam_plan3.dat detect_plan3.dat plan3.dat -e ["--beamfile=beam_plan3.dat --detectfile=detect_plan3.dat"] -s ["-p plgrid"]
+
+PARTICLES=1000000
+JOBS=100
+WALLTIME=00:59:00
+
+
+generatemc -p ${PARTICLES} -j ${JOBS} . -x beam_plan1.dat detect_plan1.dat plan1.dat -e ["--beamfile=beam_plan1.dat --detectfile=detect_plan1.dat"] -s ["-p plgrid --time ${WALLTIME}"] -w workspace1
+generatemc -p ${PARTICLES} -j ${JOBS} . -x beam_plan2.dat detect_plan2.dat plan2.dat -e ["--beamfile=beam_plan2.dat --detectfile=detect_plan2.dat"] -s ["-p plgrid --time ${WALLTIME}"] -w workspace2
+generatemc -p ${PARTICLES} -j ${JOBS} . -x beam_plan3.dat detect_plan3.dat plan3.dat -e ["--beamfile=beam_plan3.dat --detectfile=detect_plan3.dat"] -s ["-p plgrid --time ${WALLTIME}"] -w workspace3
